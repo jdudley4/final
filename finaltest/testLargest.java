16fee0a1c0de644cabcfeb470d753b6a724bde01
jdudley <jsdudley@umich.edu>
2020-12-18 16:28:38 -0500

    initial

---
+++ finaltest/testLargest.java
@@ -0,0 +1,28 @@
+package finaltest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testLargest  {
+	private Largest temp1;
+	
+	@Test
+	public void test() {
+		testPositive();
+	}
+	public void testPositive(){
+        int[] arr = new int[4];
+        arr[0] = 11;
+        arr[1] = 9;
+        arr[2] = 7;
+        arr[3] = 10;
+        temp1 = new Largest();
+        int maxValue = temp1.largest(arr);
+	/** add tests to check for this unit test **/
+        assertEquals(arr[0], maxValue);
+    }
+	
+}
+
+	
