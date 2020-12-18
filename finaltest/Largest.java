336da8643d850053b63f9297192baec8059d3243
jdudley <jsdudley@umich.edu>
2020-12-18 16:30:41 -0500

    added testPositive

--- finaltest/Largest.java
+++ finaltest/Largest.java
@@ -5,8 +5,8 @@ public class Largest {
     public int largest(int[] list){
         int index;
         int max =0;
-        for (index = 0; index < list.length-1; index++){
-            if (list[index] > max) {
+        for (index = 1; index < list.length-1; index++){
+            if (list[index] < max) {
                 max = list[index];
             }
         }
